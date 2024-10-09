2dd3e4305778a27ed7f81352bb467e96fd7199f9
Zaviere Washington <159487623+KrAceZ@users.noreply.github.com>
2024-10-09 09:54:49 -0400

    Add files via upload

--- GIPHY_API_PRACTICE.py
+++ GIPHY_API_PRACTICE.py
@@ -2,9 +2,9 @@ import requests
 
 #API_KEY
 API_KEY = "aaaaaaaaaaaaaaaaa"
-endpoint = https://api.giphy.com/v1/gifs/trending
+endpoint = "https://api.giphy.com/v1/gifs/trending"
 
-params = {"api":API_KEY, "limit":3, rating": "g"}
+params = {"api_key": API_KEY, "limit": 3, "rating": "g"}
 response = requests.get(endpoint, params=params).json()
 for gif in response["data"]:
     title = gif["title"]
